7accd544cc502b1f8698155e644366d28118c891
Abhishek Kumar Singh <abhimdp7479@gmail.com>
2025-04-22 01:59:13 +0530

    Fix: Added root route for Render

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ export const razorpay =new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_SECRET,
 })
+app.get("/", (req, res) => {
+    res.send("LMS Backend is working ✅");
+  });
+  
 app.listen(PORT,async ()=>{
     await connectionToDB();
     console.log(`App is running at  http:localhost:${PORT} `);
